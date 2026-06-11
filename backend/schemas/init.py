63b916b097c8898819c2d9d55480cd7f20d79b73
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-11 19:20:45 +0530

    Create init.py

---
+++ backend/schemas/init.py
@@ -0,0 +1,11 @@
+from .user import *
+from .identity import *
+from .threat import *
+
+from .face_analysis import *
+from .voice_analysis import *
+from .writing_analysis import *
+
+from .risk import *
+from .echovision import *
+from .upload import *
