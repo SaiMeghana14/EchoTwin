9628dc6ad5dab0be6edbb81ecf004910ac38f49a
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-12 10:51:08 +0530

    Create workflow.py

---
+++ agents/orchestrator/workflow.py
@@ -0,0 +1,30 @@
+WORKFLOW = {
+
+    "identity_creation": [
+
+        "IdentityBuilderAgent"
+
+    ],
+
+    "content_analysis": [
+
+        "FaceGuardianAgent",
+
+        "VoiceGuardianAgent",
+
+        "WritingGuardianAgent",
+
+        "RiskAgent",
+
+        "ExplainabilityAgent"
+
+    ],
+
+    "future_threats": [
+
+        "EchoVisionAgent",
+
+        "TwinShieldAgent"
+
+    ]
+}
